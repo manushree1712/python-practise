458ce8a5fad88c96de99ad2dcd5eb8dc2b011d2e
manuuuuuuuuu17 <manushree.1712@gmail.com>
2026-08-19 00:58:34 +0530

    python commit

---
+++ 01_print_function.py
@@ -0,0 +1,17 @@
+#print("hello world")
+#print(72)
+#print("3+4")
+
+
+print("manushree")
+print("cse general - 1st year")
+
+
+print("5+10 =", 10+5)
+print("10-5 =", 10-5)
+print(10*5)
+print(10/5)
+
+
+
+print("5+10 =", 5+1)
